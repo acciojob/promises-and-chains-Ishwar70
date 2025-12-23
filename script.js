c328481a42dba9258ccfde46ad74940a120ceece
Ishwar <ishwar.cse25@gmail.com>
2025-12-23 18:16:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const form = document.getElementById("form");
+const ageInput = document.getElementById("age");
+const nameInput = document.getElementById("name");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const age = ageInput.value;
+  const name = nameInput.value;
+
+  // Validation
+  if (age === "" || name === "") {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age > 18) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, 4000);
+  });
+
+  promise
+    .then(() => {
+      alert(`Welcome, ${name}. You can vote.`);
+    })
+    .catch(() => {
+      alert(`Oh sorry ${name}. You aren't old enough.`);
+    });
+});
